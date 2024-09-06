a07b92603bdd8332c4c9623bd8c1a3672d6b77bd
Admin Dans <bainandhika.baghaskara@dansmultipro.com>
2024-09-06 11:37:57 +0700

    create router

---
+++ apps/controllers/routers/router.go
@@ -0,0 +1,33 @@
+package routers
+
+import (
+	"product-ms/apps/controllers/handlers"
+	"product-ms/apps/models/repositories"
+	"product-ms/apps/models/services"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"gorm.io/gorm"
+)
+
+func SetupRouter(db *gorm.DB) *fiber.App {
+	productRepo := repositories.NewProductRepo(db)
+	productService := services.NewProductService(productRepo)
+	productHandler := handlers.NewHandler(productService)
+
+	r := fiber.New()
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins: "*",
+		AllowMethods: "GET, POST, PUT, DELETE",
+		AllowHeaders: "Content-Type, Authorization",
+	}))
+
+	r.Post("/api/v1/product/create", productHandler.CreateProduct)
+	r.Get("/api/v1/product/:id", productHandler.GetProductByID)
+	r.Get("/api/v1/product/get-all", productHandler.GetProducts)
+	r.Put("/api/v1/product/:id", productHandler.UpdateProduct)
+	r.Delete("/api/v1/product/:id", productHandler.DeleteProduct)
+
+	return r
+}
