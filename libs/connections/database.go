5c4de6fef38713d3058c950048f90067c7d203df
Admin Dans <bainandhika.baghaskara@dansmultipro.com>
2024-09-06 11:40:33 +0700

    create database connection

---
+++ libs/connections/database.go
@@ -0,0 +1,38 @@
+package connections
+
+import (
+	"fmt"
+	"log"
+	"product-ms/apps/views"
+	"product-ms/libs/configs"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+type SetMySQL struct {
+	DBConfig configs.DatabaseConfig
+}
+
+func (m *SetMySQL) SetMySQL() *gorm.DB {
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		m.DBConfig.Username,
+		m.DBConfig.Password,
+		m.DBConfig.Host,
+		m.DBConfig.Port,
+		m.DBConfig.Name,
+	)
+
+	database, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		PrepareStmt: true,
+	})
+	if err != nil {
+		log.Fatalf("Failed to connect to MySQL! err: %v", err)
+	}
+
+	if err = database.AutoMigrate(&views.Product{}); err != nil {
+		log.Fatalf("Failed to auto migrate user! err: %v", err)
+	}
+
+	return database
+}
